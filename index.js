fc7414236984e2ed8041525a26b470b46ee37cb7
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-05-28 13:14:07 -0700

    added search form

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.set('view engine', 'handlebars');
 
 // routes
 app.get('/', (req, res) => {
-  const gifurl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  res.render('hello-gif', { gifurl })
+  console.log(req.query)
+  res.render('Home')
 });
 
 app.get('/greeting/:name', (req, res) => {
